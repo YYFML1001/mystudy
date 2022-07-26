93ed5288ff771a75cf8dca6511681d0a3d2a80ba
YYFML1001 <yml062512>
2022-07-26 22:59:03 +0800

    springboottest

---
+++ firstSpringBoot/src/test/java/com/yml/mySpring/FirstSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yml.mySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
